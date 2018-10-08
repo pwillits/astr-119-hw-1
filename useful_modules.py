9e78965f4e9d8ca40ce3575b3c7b748bcead600a
pwillits <43651640+pwillits@users.noreply.github.com>
2018-10-08 10:31:59 -0700

    Add files via upload

---
+++ useful_modules.py
@@ -0,0 +1,8 @@
+import numpy as np					#the Numpy library
+import matplotlib.pyplot as plt		#matplotlib's pyplot
+
+import sys							#gives access to a C-like sys Library
+import os							#gives access to operating system
+
+print(sys.argv)		#prints any command line arguments, incl program name
+print(os.getcwd())	#prints the current working directory
